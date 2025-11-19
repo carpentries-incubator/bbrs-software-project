d24da4e17a0ca53dd35bdb216e2ea2637721d47a
Aleksandra Nenadic <a.nenadic@manchester.ac.uk>
2025-11-19 11:17:36 +0000

    Update eva_data_analysis.py

--- spacewalks/eva_data_analysis.py
+++ spacewalks/eva_data_analysis.py
@@ -25,7 +25,7 @@ def main(input_file, output_file, graph_file):
 def read_json_to_dataframe(input_file):
     """
     Read the data from a JSON file into a Pandas dataframe.
-    Clean the data by removing any incomplete rows and sort by date
+    Clean the data by removing any rows where the 'duration' value is missing.
 
     Args:
         input_file (str): The path to the JSON file.
@@ -37,7 +37,7 @@ def read_json_to_dataframe(input_file):
     # Read the data from a JSON file into a Pandas dataframe
     eva_df = pd.read_json(input_file, convert_dates=['date'], encoding="ascii")
     eva_df['eva'] = eva_df['eva'].astype(float)
-    # Clean the data by removing any incomplete rows
+    # Clean the data by removing any rows where duration is missing
     eva_df.dropna(axis=0, inplace=True)
     return eva_df
 
