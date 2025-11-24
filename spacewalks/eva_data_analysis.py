08829e1312065023cfa986d2b6cf0dac243ae94d
Aleksandra Nenadic <a.nenadic@manchester.ac.uk>
2025-11-24 20:38:05 +0000

    Update eva_data_analysis.py

--- spacewalks/eva_data_analysis.py
+++ spacewalks/eva_data_analysis.py
@@ -1,4 +1,4 @@
-import matplotlib.pyplot as plt
+mport matplotlib.pyplot as plt
 import pandas as pd
 
 def read_json_to_dataframe(input_file):
@@ -17,7 +17,7 @@ def read_json_to_dataframe(input_file):
     eva_df = pd.read_json(input_file, convert_dates=['date'], encoding='ascii')
     eva_df['eva'] = eva_df['eva'].astype(float)
     # Clean the data by removing any rows where duration is missing
-    eva_df.dropna(axis=0, inplace=True)
+    eva_df.dropna(axis=0, subset=['duration', 'date'], inplace=True)
     return eva_df
 
 
