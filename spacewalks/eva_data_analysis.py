e57e6080905557dec27801a08fd6a2753e9a0a90
Aleksandra Nenadic <a.nenadic@manchester.ac.uk>
2025-11-24 21:26:48 +0000

    Update spacewalks/eva_data_analysis.py

--- spacewalks/eva_data_analysis.py
+++ spacewalks/eva_data_analysis.py
@@ -52,7 +52,7 @@ def write_dataframe_to_csv(df, output_file):
 
     Args:
         df (pd.DataFrame): The input dataframe.
-        output_file (str): The path to the output CSV file.
+        output_file (file or str): The file object or path to the output CSV file.
 
     Returns:
         (None):
