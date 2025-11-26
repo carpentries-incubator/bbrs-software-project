c81d52f9aeaff192c0b09dad29d269b22408a837
Aleksandra Nenadic <a.nenadic@manchester.ac.uk>
2025-11-26 12:21:57 +0000

    Update eva_data_analysis.py

--- spacewalks/eva_data_analysis.py
+++ spacewalks/eva_data_analysis.py
@@ -67,6 +67,7 @@ def plot_cumulative_time_in_space(df, graph_file):
 
 print("--START--")
 
+# https://data.nasa.gov/resource/eva.json (with modifications)
 input_file = open('./eva-data.json', 'r', encoding='ascii')
 output_file = open('./eva-data.csv', 'w', encoding='utf-8')
 graph_file = './cumulative_eva_graph.png'
